3abca06d068494e98f80fffc32cea12abd86b329
isshin123 <isshin95@gmail.com>
2022-06-10 14:44:16 +0900

    Basic Settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
