1411cd27cb379466e9e4bd8fff6fbde0b1128725
isshin123 <isshin95@gmail.com>
2022-06-10 14:49:29 +0900

    basic pages route

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<NotFound />}></Route>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/add" element={<Add />}></Route>\
+        <Route path="/book/:id" element={<Detail />}></Route>
+        <Route path="/edit/:id" element={<Edit />}></Route>
       </Routes>
     </BrowserRouter>
   );
