333f192f5a1dbccb71949729f7d74557e3e8965c
isshin123 <isshin95@gmail.com>
2022-06-10 15:16:54 +0900

    Error Component

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,22 @@ import Signin from "./pages/Signin";
 import Add from "./pages/Add";
 import Detail from "./pages/Detail";
 import Edit from "./pages/Edit";
+import Error from "./pages/Error";
+import { ErrorBoundary } from "react-error-boundary";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<NotFound />}></Route>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/add" element={<Add />}></Route>\
-        <Route path="/book/:id" element={<Detail />}></Route>
-        <Route path="/edit/:id" element={<Edit />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<NotFound />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/add" element={<Add />}></Route>\
+          <Route path="/book/:id" element={<Detail />}></Route>
+          <Route path="/edit/:id" element={<Edit />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
